61b9b135a67e78575550636dc93f7e8990f51da6
Abh-I-sek <abhisek211004@gmail.com>
2024-06-22 13:32:46 +0530

    import fixed3

--- src/_component/CodeSpace.tsx
+++ src/_component/CodeSpace.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 import CodeMirror from '@uiw/react-codemirror';
 import { useState,useCallback } from 'react';
 import { Button } from '~/components/ui/button';
@@ -6,21 +6,21 @@ import { Copy } from 'lucide-react';
 import { materialDark } from '@uiw/codemirror-theme-material';
 
 export default function CodeSpace(props : {code : string}){
-    // const [code , setCode] = useState<string>(props.code);
+    const [code , setCode] = useState<string>(props.code);
 
-    // const update = useCallback((val: string) => {
-    //     setCode(val);
-    // }, []);
+    const update = useCallback((val: string) => {
+        setCode(val);
+    }, []);
 
     return(
         <div>
         <div className="text-center mb-3">
-            {/* <Button onMouseDown={()=> navigator.clipboard.writeText(code)}>Copy <Copy className='w-4 ml-1'/></Button> */}
-            <Button>Copy <Copy className='w-4 ml-1'/></Button>
+            <Button onMouseDown={()=> navigator.clipboard.writeText(code)}>Copy <Copy className='w-4 ml-1'/></Button>
+            {/* <Button>Copy <Copy className='w-4 ml-1'/></Button> */}
         
         </div>
-        {/* <CodeMirror height="500px" value={props.code} ></CodeMirror> */}
-        <textarea className='w-full h-full'>{props.code}</textarea>
+        <CodeMirror height="500px" value={props.code} theme={materialDark}></CodeMirror>
+        {/* <textarea className='w-full h-full'>{props.code}</textarea> */}
         </div>  
     )
 
