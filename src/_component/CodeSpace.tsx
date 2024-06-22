e4f43fd59deb4217581b221c188476ff71bb48e9
Abh-I-sek <abhisek211004@gmail.com>
2024-06-22 13:36:11 +0530

    fixed copying_bug

--- src/_component/CodeSpace.tsx
+++ src/_component/CodeSpace.tsx
@@ -14,13 +14,11 @@ export default function CodeSpace(props : {code : string}){
 
     return(
         <div>
-        <div className="text-center mb-3">
-            <Button onMouseDown={()=> navigator.clipboard.writeText(code)}>Copy <Copy className='w-4 ml-1'/></Button>
-            {/* <Button>Copy <Copy className='w-4 ml-1'/></Button> */}
-        
-        </div>
-        <CodeMirror height="500px" value={props.code} theme={materialDark}></CodeMirror>
-        {/* <textarea className='w-full h-full'>{props.code}</textarea> */}
+            <div className="text-center mb-3">
+                <Button onMouseDown={()=> navigator.clipboard.writeText(code)}>Copy <Copy className='w-4 ml-1'/></Button>
+            
+            </div>
+            <CodeMirror height="500px" value={code} onChange={update} theme={materialDark}></CodeMirror>
         </div>  
     )
 
