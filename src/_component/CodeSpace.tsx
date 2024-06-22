13df6d42c0f3894314ba700c24a2643d111f5fb1
Abh-I-sek <abhisek211004@gmail.com>
2024-06-23 00:06:52 +0530

    resolved a typo

--- src/_component/CodeSpace.tsx
+++ src/_component/CodeSpace.tsx
@@ -15,7 +15,7 @@ export default function CodeSpace(props : {code : string}){
     return(
         <div>
             <div className="text-center mb-3">
-                <Button onMouseDown={()=> navigator.clipboard.writeText(code)}>Copy <Copy className='w-4 ml-1'/></Button>\
+                <Button onMouseDown={()=> navigator.clipboard.writeText(code)}>Copy <Copy className='w-4 ml-1'/></Button>
             </div>
             <CodeMirror height="500px" value={code} onChange={update} theme={materialDark}></CodeMirror>
         </div>  
